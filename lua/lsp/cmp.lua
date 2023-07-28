763278c6b8e4ef3ab4608381dce8a3b64279dd5b
Austin Wallace <austinwallace196@gmail.com>
2023-07-27 22:47:39 -0400

    neotest imrpovements

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -1,11 +1,11 @@
 vim.g.completeopt = "menu,menuone,noselect,noinsert"
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and
-               vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col,
-                                                                          col)
-                   :match("%s") == nil
-end
+-- local has_words_before = function()
+--     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+--     return col ~= 0 and
+--                vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col,
+--                                                                           col)
+--                    :match("%s") == nil
+-- end
 local cmp = require 'cmp'
 local lspkind = require('lspkind')
 local ls = require("luasnip")
